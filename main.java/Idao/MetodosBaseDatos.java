9817e7a27550bd3edb15acbeb10443aa9a24923a
APachon10 <Pachondestroy95@gmail.com>
2019-01-25 17:12:51 +0100

    paquetes mejor organizados

--- main.java/Idao/MetodosBaseDatos.java
+++
@@ -1,8 +0,0 @@
-package Idao;
-
-public interface MetodosBaseDatos {
-	public void añadirUsuario();
-	public void comprovarUser();
-	public void comprovarPassword();
-	public void añadirProyecto();
-}
---
+++ src/main.java/Idao/MetodosBaseDatos.java
@@ -0,0 +1,8 @@
+package Idao;
+
+public interface MetodosBaseDatos {
+	public void añadirUsuario();
+	public void comprovarUser();
+	public void comprovarPassword();
+	public void añadirProyecto();
+}
